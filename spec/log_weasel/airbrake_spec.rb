140343f38d9648b01b11655440b1700d6810a1ab
Brett Fishman <brett.fishman@stitchfix.com>
2017-04-10 11:21:25 -0700

    Remove deprecated notify_or_ignore method. Revise spec for current RSpec syntax.

--- spec/log_weasel/airbrake_spec.rb
+++ spec/log_weasel/airbrake_spec.rb
@@ -4,15 +4,17 @@ require 'airbrake'
 describe LogWeasel::Airbrake do
   before do
     LogWeasel.configure {}
-    Airbrake.configure {}
+    Airbrake.configure do |c|
+      c.project_id = 99999
+      c.project_key = "bogus-1234"
+    end
     LogWeasel::Transaction.stubs(:id).returns('123')
   end
 
   it "adds transaction id to parameters with no parameters" do
-    Airbrake.expects(:send_notice).with do |notice|
-      notice.parameters.should have_key('log_weasel_id')
+    Airbrake.expects(:notify_without_transaction_id).with do |_, opts|
+      expect(opts[:parameters].keys).to include('log_weasel_id')
     end
     Airbrake.notify(RuntimeError.new('failure'))
   end
-
 end
