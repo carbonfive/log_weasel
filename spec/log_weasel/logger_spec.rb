5a36b794e69144bd3cb1026cb94fe35b5236360f
Brett Fishman <brett.fishman@stitchfix.com>
2017-04-10 11:31:11 -0700

    Update to ActiveSupport::Logger.

--- spec/log_weasel/logger_spec.rb
+++ spec/log_weasel/logger_spec.rb
@@ -1,14 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe LogWeasel::BufferedLogger do
+describe LogWeasel::Logger do
   before do
     @stringio = StringIO.new
-    @logger   = LogWeasel::BufferedLogger.new @stringio
+    @logger   = LogWeasel::Logger.new @stringio
     LogWeasel::Transaction.stubs(:id).returns('123')
   end
 
   it "logs transaction id" do
     @logger.info 'message'
-    @stringio.string.should =~ / 123 /
+    expect(@stringio.string).to match(/ 123 /)
   end
 end
