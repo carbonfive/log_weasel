df6c810cc2a8476a9014f21298f50f0f68ecbfca
Brett Fishman <brett.fishman@stitchfix.com>
2017-05-18 16:24:08 -0700

    Add spec coverage for Railtie. Use combustion gem. Call class method properly. [#145729071]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
+require 'combustion'
 require 'stitch_fix/log_weasel'
+
+Combustion.initialize! :active_support
+
 require 'rspec'
 
 begin
