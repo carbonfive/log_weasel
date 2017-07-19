8f98cb3924e8482fb79d8c0d983de0425dc7f641
Brett Fishman <brett.fishman@stitchfix.com>
2017-07-19 01:22:20 -0700

    Rename task.

--- lib/stitch_fix/log_weasel/tasks.rb
+++ lib/stitch_fix/log_weasel/tasks.rb
@@ -4,12 +4,12 @@ require 'resque/tasks'
 require 'resque-scheduler'
 
 namespace :resque do
-  desc "Add Log Weasel setup to resque:scheduler setup task"
-  task :add_log_weasel_setup do
+  desc "Adds Log Weasel setup to resque:scheduler setup task"
+  task :log_weasel_setup do
     StitchFix::LogWeasel::ResqueScheduler.initialize!
   end
 end
 
 Rake::Task['resque:scheduler_setup'].enhance do
-  Rake::Task['resque:add_log_weasel_setup'].invoke
+  Rake::Task['resque:log_weasel_setup'].invoke
 end
