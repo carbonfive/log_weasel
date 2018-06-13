1269e6dc6ac18c75166aa300d6c8569fded3cd7f
Brett Fishman <brett.fishman@stitchfix.com>
2018-06-13 11:20:59 -0700

    Remove check for Rails version, as we don't support versions < 5.

--- lib/stitch_fix/log_weasel/logger.rb
+++ lib/stitch_fix/log_weasel/logger.rb
@@ -3,12 +3,7 @@ require 'logger'
 module StitchFix
   class LogWeasel::Logger < ::Logger
     if defined?(Rails)
-      # include LoggerSilence for Rails versions >= 5
-      requirement = Gem::Requirement.new('>= 5.0.0')
-      version = Gem.loaded_specs['rails'].version
-      if requirement.satisfied_by?(version)
-        include LoggerSilence
-      end
+      include LoggerSilence
     end
 
     def add(severity, message = nil, progname = nil, &block)
